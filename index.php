72009c488a022491d570d4845a69ea31bad1da16
kittenmasque <volaju@gmail.com>
2023-02-11 15:29:48 -0800

    created form in index for calculating 2 numbers

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="functions.php" method="get">
+    <input type="text" name="num01" placeholder="Number 1">
+    <select name="oper">
+        <label>Choose operation!</label>
+        <option value="add">Add</option>
+        <option value="sub">Subtract</option>
+        <option value="multiply">Multiply</option>
+        <option value="divide">Divide</option>
+</select>
+<input type="text" name="num02" placeholder="Number 2">
+<button type="submit">Calcluate!</button>
+</form>
+
+</body>
+</html>
